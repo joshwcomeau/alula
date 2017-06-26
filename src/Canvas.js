3c4596862ef3a4a36233dfa213e383a5a1143bef
Joshua Comeau <joshwcomeau@gmail.com>
2017-06-26 08:30:43 -0400

    Fiddle around without luck :/

--- src/Canvas.js
+++ src/Canvas.js
@@ -105,109 +105,66 @@ class Canvas extends PureComponent {
     // as a 'preview'. The state isn't saved until the drag is released.
     this.history.restore(this.canvas, this.ctx);
 
+    this.ctx.save();
+
+
     const {x1, y1} = this;
     const {x: x2, y: y2} = this.getEventCoords(ev);
 
     const sideA = x2 - x1;
     const sideB = y2 - y1;
 
-    // Extend our line so that it reaches the edge of the canvas.
-    // Given that this is just a straight line, it can be represented by
-    // the simple equation `y = ax + b`.
-    // We can figure out the slope, `a`, pretty easily.
-    const slope = sideB / sideA;
-    // We can work out the offset `b` by solving the equation for one of our
-    // known points.
-    // y1 = slope * x1 + offset
-    const offset = y1 - (slope * x1);
+    let fullLine;
 
-    // Now that we have this data, extend our line to span the entire canvas.
-    const originLine = {
-      x1: 0 * this.pixelRatio,
-      y1: offset * this.pixelRatio,
-      x2: this.canvas.width * this.pixelRatio,
-      y2: (slope * this.canvas.width + offset) * this.pixelRatio,
-    };
+    // If our line is perfectly vertical, the standard `ax + b` form won't
+    // work. In this case, though, our job is easy.
+    if (sideA === 0) {
+      fullLine = {
+        x1: x1 * this.pixelRatio,
+        y1: 0,
+        x2: x2 * this.pixelRatio,
+        y2: this.canvas.height,
+      };
 
-    this.ctx.save();
+      console.log('Setting transform')
+      this.ctx.translate(this.canvas.width / this.pixelRatio, 0);
+      this.ctx.scale(-1 / this.pixelRatio, 1 / this.pixelRatio);
 
-    this.ctx.setTransform(...mirrorTransformLine(originLine));
+      this.ctx.beginPath();
+      this.ctx.moveTo(this.canvas.width - fullLine.x1, 0);
+      this.ctx.lineTo(this.canvas.width - fullLine.x2, this.canvas.height);
+      this.ctx.lineTo(0, this.canvas.height);
+      this.ctx.lineTo(0, 0);
+      this.ctx.lineTo(this.canvas.width - fullLine.x1, 0);
+    } else {
+      // Extend our line so that it reaches the edge of the canvas.
+      // Given that this is just a straight line, it can be represented by
+      // the simple equation `y = ax + b`.
+      // We can figure out the slope, `a`, pretty easily.
+      const slope = sideB / sideA;
+      // We can work out the offset `b` by solving the equation for one of our
+      // known points.
+      // y1 = slope * x1 + offset
+      const offset = y1 - (slope * x1);
 
-    // Create a path that only covers the reflected part of the line.
-    // This is a surprisingly tricky thing, since the "side" we want to clip
-    // depends on the current line's two points (we generally want to reflect
-    // over the smaller of the two sides bisected by this line).
-    //
-    // First, figure out which direction is shorter, clockwise or counter-cw.
-    function calculatePerimeter(args) {
-      const {originalPoint, currentPoint, direction, size, sum = 0} = args;
+      // Now that we have this data, extend our line to span the entire canvas.
+      fullLine = {
+        x1: 0 * this.pixelRatio,
+        y1: offset * this.pixelRatio,
+        x2: this.canvas.width * this.pixelRatio,
+        y2: (slope * this.canvas.width + offset) * this.pixelRatio,
+      };
 
-      // Are we on the same axis as our final point?
-      if (direction === 'ccw') {
-        if (
-          currentPoint.x === 0 && currentPoint.y === 0 &&
-          originalPoint.x === 0
-        ) {
-          return sum + originalPoint.y;
-        }
+      this.ctx.setTransform(...mirrorTransformLine(fullLine));
 
-        if (
-          currentPoint.x === 0 && currentPoint.y === size &&
-          originalPoint.y === size
-        ) {
-          return sum + originalPoint.x;
-        }
-
-        if (
-          currentPoint.x === size && currentPoint.y === 0 &&
-          originalPoint.y === 0
-        ) {
-          return sum + (size - originalPoint.x);
-        }
-      } else {
-        if (
-          currentPoint.x === 0 && currentPoint.y === size &&
-          originalPoint.x === 0
-        ) {
-          return sum + (size - originalPoint.y);
-        }
-
-        if (
-          currentPoint.x === size && currentPoint.y === size &&
-          originalPoint.y === size
-        ) {
-          return sum + (size - originalPoint.x);
-        }
-
-        if (
-          currentPoint.x === 0 && currentPoint.y === 0 &&
-          originalPoint.y === 0
-        ) {
-          return sum + originalPoint.x;
-        }
-      }
-
-      // If not, add the size to our sum, and move the currentPoint to
-      // the next corner.
-      const corners = [
-        { x: 0, y: 0 },
-        { x: size, y: 0 },
-        { x: size, y: size },
-        { x: 0, y: size },
-      ];
-
-      return calculatePerimeter({
-        ...args,
-      })
+      this.ctx.beginPath();
+      this.ctx.moveTo(fullLine.x1, fullLine.y1);
+      this.ctx.lineTo(fullLine.x2, fullLine.y2);
+      this.ctx.lineTo(this.canvas.width, 0);
+      this.ctx.lineTo(0, 0);
     }
 
-    // this.ctx.beginPath();
-    // this.ctx.moveTo(originLine.x1, originLine.y1);
-    // this.ctx.lineTo(originLine.x2, originLine.y2);
-    // this.ctx.lineTo(this.canvas.width, 0);
-    // this.ctx.lineTo(0, 0);
-    //
-    // this.ctx.clip();
+    this.ctx.clip();
 
     // draw the image
     // since the this.ctx is rotated, the image will be rotated also
