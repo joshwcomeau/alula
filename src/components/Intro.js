ed6633a8c22a66741cd7feae6fdd6b6a531287df
Joshua Comeau <joshwcomeau@gmail.com>
2017-07-04 18:05:43 -0400

    Add favicon, tweak intro landscape spacing

--- src/components/Intro.js
+++ src/components/Intro.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 
-import {colors, styles} from '../constants';
+import {colors, styles, media} from '../constants';
 import demoGIFUrl from '../assets/demo-2.gif';
 
 import ImageUploader from './ImageUploader';
@@ -18,19 +18,22 @@ class Intro extends Component {
     return (
       <Background>
         <Wrapper>
-          <span>
-            <Title>alula</Title>
-            <Subtitle>art via reflection</Subtitle>
-          </span>
+          <MainContent>
+            <span>
+              <Title>alula</Title>
+              <Beta>beta</Beta>
+              <Subtitle>art via reflection</Subtitle>
+            </span>
 
-          <Container>
-            <DemoGIF src={demoGIFUrl} />
-          </Container>
+            <Container>
+              <DemoGIF src={demoGIFUrl} />
+            </Container>
 
-          <ButtonContainer>
-            <ImageUploader />
-            <RandomImageSelector />
-          </ButtonContainer>
+            <ButtonContainer>
+              <ImageUploader />
+              <RandomImageSelector />
+            </ButtonContainer>
+          </MainContent>
 
           <Footer>
             © 2017-present. A thing by
@@ -57,39 +60,78 @@ const Wrapper = styled.div`
   justify-content: space-between;
   text-align: center;
   color: ${colors.grays[0]};
+`;
+
+const MainContent = styled.div`
+  flex: 1;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  max-height: 750px;
 `
 
 const Title = styled.h1`
-  font-size: 60px;
+  font-size: 64px;
   font-weight: bold;
   letter-spacing: -2.5px;
   color: ${colors.white};
   margin-top: ${styles.paddingUnitPx};
-  margin-bottom: 6px;
+  margin-bottom: 4px;
 `;
 
 const Subtitle = styled.h3`
-  font-size: 18px;
+  font-size: 16px;
   font-weight: normal;
   color: ${colors.grays[1]};
   margin-bottom: ${styles.paddingUnitPx};
 `;
 
+const Beta = styled.span`
+  position: absolute;
+  display: inline-block;
+  top: 18px;
+  margin-left: auto;
+  margin-right: auto;
+  font-size: 10px;
+  font-weight: bold;
+  color: ${colors.reds[2]};
+  transform: translateX(200%);
+`
+
 const Container = styled.div`
+  position: relative;
+  width: 80%;
+  height: 0;
+  padding-bottom: 80%;
+  margin-left: auto;
+  margin-right: auto;
+
+  ${media.xs`
+    width: 70%;
+    padding-bottom: 70%;
+  `}
+`;
+
+const ButtonContainer = styled.div`
   width: 80%;
   margin-left: auto;
   margin-right: auto;
-`;
 
-const ButtonContainer = Container.extend`
   margin-top: ${styles.paddingUnitPx};
   margin-bottom: ${styles.paddingUnit * 2}px;
+
+  ${media.xs`
+    width: 70%;
+  `}
 `
 
 const DemoGIF = styled.img`
+  position: absolute;
+  top: 0;
+  left: 0;
   display: block;
   width: 100%;
-  max-height: 40%;
+  height: 100%;
   border-radius: 2px;
 `;
 
